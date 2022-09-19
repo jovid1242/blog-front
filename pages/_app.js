ec3bdeb21c4d342eee29e2a52567572fff7c0898
jovid1242 <jovid1242@gmail.com>
2022-09-19 23:40:33 +0500

    ad yandex mt

--- pages/_app.js
+++ pages/_app.js
@@ -5,17 +5,23 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
+      {/* <Script
         strategy="lazyOnload"
         src="https://www.googletagmanager.com/gtag/js?id=G-3GPGC9LTM9"
-      />
-      <Script id="ga-analytics">
+      /> */}
+      <Script id="ya-analytics">
         {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            gtag('config', 'G-3GPGC9LTM9');
+            ym(90475706, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true
+            });
           `}
       </Script>
       <Component {...pageProps} />
