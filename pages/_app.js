6ea1e6e46b228387af9b5b08169356c7de4944e0
jovid1242 <jovid1242@gmail.com>
2022-09-19 17:39:02 +0500

    add google analitics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
 import "../styles/globals.css";
 import { wrapper } from "../redux/store";
+import Script from "next/script";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-3GPGC9LTM9"
+      />
+      <Script id="ga-analytics">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-3GPGC9LTM9');
+          `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
