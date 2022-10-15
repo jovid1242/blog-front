56da3f4ecc84bb49916c40f005d2b67af901f2ba
jovid1242 <jovid1242@gmail.com>
2022-10-15 07:56:01 +0500

    tags

--- components/instagram/index.js
+++ components/instagram/index.js
@@ -5,72 +5,75 @@ const Insta = () => {
     <>
       <div className="instagram">
         <div className="container-xl">
-          <a href="#" className="btn btn-default btn-instagram">
+          <a
+            href="https://instagram.com/jovid92002"
+            className="btn btn-default btn-instagram"
+          >
             @Repost on Instagram
           </a>
           <div className="instagram-feed d-flex flex-wrap">
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
                   src="/static/insta-1.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
             </div>
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
                   src="/static/insta-2.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
             </div>
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
                   src="/static/insta-3.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
             </div>
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
                   src="/static/insta-4.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
             </div>
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
                   src="/static/insta-5.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
             </div>
             <div className="insta-item col-sm-2 col-6 col-md-2">
-              <a href="#">
+              <a href="https://instagram.com/jovid92002">
                 <Image
-                  src="/static/insta-5.jpg"
+                  src="/static/insta-6.jpg"
                   alt="insta-title"
                   width={1000}
-                  height={600}
+                  height={1000}
                   priority
                 />
               </a>
