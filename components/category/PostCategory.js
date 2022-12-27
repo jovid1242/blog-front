b514aba3df38d0d48b737a2292847d24c1061040
Jovid1242 <jovid1242@gmail.com>
2022-12-27 12:17:59 +0300

    add delete post

--- components/category/PostCategory.js
+++ components/category/PostCategory.js
@@ -1,125 +1,167 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchAuthors } from "../../redux/slices/users"; 
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchAuthors } from '../../redux/slices/users'
+import { DeleteOutlined } from '@ant-design/icons'
+import { toast } from 'react-toastify'
 
 // next
-import Link from "next/link";
-import { useRouter } from "next/router";
-import Image from "next/image";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Image from 'next/image'
 
 // utils
-import { getAuthors } from "../../utils/author";
-import { translitRuEnLowercase } from "../../utils/translateUrl";
+import { getAuthors } from '../../utils/author'
+import { translitRuEnLowercase } from '../../utils/translateUrl'
+
+import { setLoad } from '../../redux/slices/author/author'
 
 // api
-import { API_URL } from "../api";
+import { API_URL } from '../api'
+import http from '../http'
 
 // momentjs
-import * as moment from "moment";
-import "moment/locale/ru";
+import * as moment from 'moment'
+import 'moment/locale/ru'
 
-moment.locale("ru");
+moment.locale('ru')
 
-const PostCategory = ({ post, col = "col-sm-6" }) => {
-  const router = useRouter();
-  const dispatch = useDispatch();
- 
+const PostCategory = ({ post, col = 'col-sm-6', status }) => {
+    const router = useRouter()
+    const dispatch = useDispatch()
 
-  const { category } = useSelector((state) => state.category);
-  const { users } = useSelector((state) => state.users);
+    const { category } = useSelector((state) => state.category)
+    const { users } = useSelector((state) => state.users) 
+    const user = getAuthors.getAuthor(users.items, post.user_id)
 
-  const user = getAuthors.getAuthor(users.items, post.user_id);
+    const filterCategory = category?.items.filter(
+        (elm) => elm.id == router.query.id
+    )
+    useEffect(() => {
+        dispatch(fetchAuthors())
+    }, [])
 
-  const filterCategory = category?.items.filter(
-    (elm) => elm.id == router.query.id
-  );
+    const removePostById = (id) => {
+        dispatch(setLoad(true))
+        http.delete(`/author/post/${id}`)
+            .then((res) => { 
+              window.location.reload()
+                toast.success(res.data.ok)
+            })
+            .catch((err) => {
+                toast.error(err)
+            })
+            .finally(() => {
+                dispatch(setLoad(false))
+            })
+    }
 
-  useEffect(() => {
-    dispatch(fetchAuthors());
-  }, []);
+    return (
+        <div className={col}>
+            <div className="post post-grid rounded bordered mb-2">
+                <div className="thumb top-rounded">
+                    {status ? (
+                        <div
+                            className="category-badge position-absolute cursorp"
+                            onClick={() => removePostById(post.id)}
+                        >
+                            <DeleteOutlined />
+                        </div>
+                    ) : (
+                        <Link href={`/category/${router.query.id}`}>
+                            <a className="category-badge position-absolute">
+                                {filterCategory[0]?.title}
+                            </a>
+                        </Link>
+                    )}
 
-  return (
-    <div className={col}>
-      <div className="post post-grid rounded bordered mb-2">
-        <div className="thumb top-rounded">
-          <Link href={`/category/${router.query.id}`}>
-            <a className="category-badge position-absolute">
-              {filterCategory[0]?.title}
-            </a>
-          </Link>
+                    <span className="post-format">
+                        <i className="icon-picture"></i>
+                    </span>
+                    <Link
+                        href={`/post/${translitRuEnLowercase(post.title)}/${
+                            post.id
+                        }`}
+                    >
+                        <a>
+                            <div className="inner">
+                                <Image
+                                    src={`${API_URL}image/${post.imageUrl}`}
+                                    className="imgCover"
+                                    width={1000}
+                                    height={600}
+                                    priority
+                                    alt="post-title"
+                                />
+                            </div>
+                        </a>
+                    </Link>
+                </div>
+                <div className="details">
+                    <ul className="meta list-inline mb-3 d-flex align-items-center">
+                        <li className="list-inline-item d-flex align-items-center">
+                            <Link href={`/author/${post.user_id}`}>
+                                <a>
+                                    <Image
+                                        src={
+                                            !user.imageUrl
+                                                ? '/static/insta-2.jpg'
+                                                : `${API_URL}image/${user.imageUrl}`
+                                        }
+                                        className="w40 mr-2 avatar-img border50"
+                                        alt="author"
+                                        width={40}
+                                        height={40}
+                                        layout="intrinsic"
+                                    />
+                                </a>
+                            </Link>
 
-          <span className="post-format">
-            <i className="icon-picture"></i>
-          </span>
-          <Link href={`/post/${translitRuEnLowercase(post.title)}/${post.id}`}>
-            <a>
-              <div className="inner">
-                <Image
-                  src={`${API_URL}image/${post.imageUrl}`}
-                  className="imgCover"
-                  width={1000}
-                  height={600}
-                  priority
-                  alt="post-title"
-                />
-              </div>
-            </a>
-          </Link>
+                            <div style={{ marginLeft: '12px' }}>
+                                <Link href={`/author/${post.user_id}`}>
+                                    <a>
+                                        {getAuthors.getAuthorName(
+                                            users.items,
+                                            post.user_id
+                                        )}
+                                    </a>
+                                </Link>
+                            </div>
+                        </li>
+                        <li className="list-inline-item d-flex align-items-center">
+                            <Link href="/">
+                                <a>
+                                    <Image
+                                        src="/static/eye-solid.svg"
+                                        alt="eye icon"
+                                        width={10}
+                                        height={10}
+                                    />
+                                </a>
+                            </Link>
+                            <div style={{ marginLeft: '6px' }}>
+                                {post.viewCount}
+                            </div>
+                        </li>
+                        <li
+                            className="list-inline-item"
+                            style={{ fontSize: '12px' }}
+                        >
+                            {moment(post.createdAt).format('LL')}
+                        </li>
+                    </ul>
+                    <h5 className="post-title">
+                        <Link
+                            href={`/post/${translitRuEnLowercase(post.title)}/${
+                                post.id
+                            }`}
+                        >
+                            <a>{post.title}</a>
+                        </Link>
+                    </h5>
+                </div>
+            </div>
         </div>
-        <div className="details">
-          <ul className="meta list-inline mb-3 d-flex align-items-center">
-            <li className="list-inline-item d-flex align-items-center">
-              <Link href={`/author/${post.user_id}`}>
-                <a>
-                  <Image
-                    src={
-                      !user.imageUrl
-                        ? "/static/insta-2.jpg"
-                        : `${API_URL}image/${user.imageUrl}`
-                    }
-                    className="w40 mr-2 avatar-img border50"
-                    alt="author"
-                    width={40}
-                    height={40}
-                    layout="intrinsic"
-                  />
-                </a>
-              </Link>
+    )
+}
 
-              <div style={{ marginLeft: "12px" }}>
-                <Link href={`/author/${post.user_id}`}>
-                  <a>{getAuthors.getAuthorName(users.items, post.user_id)}</a>
-                </Link>
-              </div>
-            </li>
-            <li className="list-inline-item d-flex align-items-center">
-              <Link href="/">
-                <a>
-                  <Image
-                    src="/static/eye-solid.svg"
-                    alt="eye icon"
-                    width={10}
-                    height={10}
-                  />
-                </a>
-              </Link>
-              <div style={{ marginLeft: "6px" }}>{post.viewCount}</div>
-            </li>
-            <li className="list-inline-item" style={{ fontSize: "12px" }}>
-              {moment(post.createdAt).format("LL")}
-            </li>
-          </ul>
-          <h5 className="post-title">
-            <Link
-              href={`/post/${translitRuEnLowercase(post.title)}/${post.id}`}
-            >
-              <a>{post.title}</a>
-            </Link>
-          </h5>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default PostCategory;
+export default PostCategory
--- pages/author/[id].js
+++ pages/author/[id].js
@@ -1,88 +1,104 @@
-import { useEffect } from "react";
-import Head from "next/head";
-import { useDispatch } from "react-redux";
-import { fetchCategory } from "../../redux/slices/category";
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import { useDispatch } from 'react-redux'
+import { fetchCategory } from '../../redux/slices/category'
 
 // api
-import http from "../../components/http";
+import http from '../../components/http'
 
 // components
-import Header from "../../components/header/Header";
-import Footer from "../../components/footer/Footer";
-import AuthorBanner from "../../components/author/authorBanner";
-import PostCategory from "../../components/category/PostCategory";
+import Header from '../../components/header/Header'
+import Footer from '../../components/footer/Footer'
+import AuthorBanner from '../../components/author/authorBanner'
+import PostCategory from '../../components/category/PostCategory'
 
 const Index = (props) => {
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch(fetchCategory());
-  }, []);
-  return (
-    <div>
-      <Head>
-        <meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>Repost</title>
-        <meta name="description" content="Repost" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1"
-        />
-      </Head>
-      <div className="site-wrapper">
-        <div className="main-overlay"></div>
-        <Header />
-        <AuthorBanner
-          name={props.user.user.name}
-          info={props.user.user?.info}
-          avatar={props.user.user?.imageUrl}
-        />
-        <section className="main-content">
-          <div className="container-md">
-            <div className="row">
-              {props.post?.rows.length > 0 ? (
-                props.post.rows.map((post) => {
-                  return (
-                    <PostCategory
-                      post={post}
-                      key={post.id}
-                      col="col-sm-4 col-md-4"
-                    />
-                  );
-                })
-              ) : (
-                <>
-                  <h2>Здесь пока что нет ни одного поста</h2>
-                </>
-              )}
+    const [chekUser, setChekUser] = useState(false)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(fetchCategory())
+    }, [])
+
+    const getUser = () => {
+        http.get('/auth/me').then((res) => {
+            setChekUser(props.user.user.id === res.data.user.id ? true : false)
+        })
+    }
+
+    useEffect(() => {
+        getUser()
+    }, [])
+
+    return (
+        <div>
+            <Head>
+                <meta
+                    httpEquiv="Content-Type"
+                    content="text/html; charset=UTF-8"
+                />
+                <title>Repost</title>
+                <meta name="description" content="Repost" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, maximum-scale=1"
+                />
+            </Head>
+            <div className="site-wrapper">
+                <div className="main-overlay"></div>
+                <Header />
+                <AuthorBanner
+                    name={props.user.user.name}
+                    info={props.user.user?.info}
+                    avatar={props.user.user?.imageUrl}
+                />
+                <section className="main-content">
+                    <div className="container-md">
+                        <div className="row">
+                            {props.post?.rows.length > 0 ? (
+                                props.post.rows.map((post) => {
+                                    return (
+                                        <PostCategory
+                                            post={post}
+                                            status={chekUser}
+                                            key={post.id}
+                                            col="col-sm-4 col-md-4"
+                                        />
+                                    )
+                                })
+                            ) : (
+                                <>
+                                    <h2>Здесь пока что нет ни одного поста</h2>
+                                </>
+                            )}
+                        </div>
+                    </div>
+                </section>
+                <Footer />
             </div>
-          </div>
-        </section>
-        <Footer />
-      </div>
-    </div>
-  );
-};
-
-export async function getServerSideProps({ params }) {
-  let post = {};
-  let user = {};
-  await http.get(`/author/${params.id}/posts`).then((res) => {
-    post = res.data.posts;
-  });
-  await http.get(`/user/${params.id}`).then((res) => {
-    user = res.data;
-  });
-
-  if (Object.keys(post).length == 0) {
-    return {
-      notFound: true,
-    };
-  }
-
-  return {
-    props: { post, user }, // will be passed to the page component as props
-  };
+        </div>
+    )
 }
 
-export default Index;
+export async function getServerSideProps({ params }) {
+    let post = {}
+    let user = {}
+    await http.get(`/author/${params.id}/posts`).then((res) => {
+        post = res.data.posts
+    })
+    await http.get(`/user/${params.id}`).then((res) => {
+        user = res.data
+    })
+
+    if (Object.keys(post).length == 0) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: { post, user }, // will be passed to the page component as props
+    }
+}
+
+export default Index
